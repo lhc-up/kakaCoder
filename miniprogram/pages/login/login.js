c088d0528dffc101baa43936b48adadb14d2a8bf
luohao <haoluo2@iflytek.com>
2020-03-27 21:55:41 +0800

    增加token登录

--- miniprogram/pages/login/login.js
+++ miniprogram/pages/login/login.js
@@ -3,19 +3,28 @@
  * @author: haoluo2
  * @date: 2020-03-05
 */
-
 import utils from '../../utils/util.js';
 import request from '../../utils/request.js';
 import CONST from '../../utils/const.js';
 Page({
     data: {
         username: '',
-        password: ''
+        password: '',
+        token: '',
+        currentIndex: 1
     },
     // 监听输入
     onInput(e) {
         const type = e.target.dataset.type;
         this.data[type] = e.detail.value;
+    },
+    // 点击登录按钮
+    handleLogin() {
+        if (this.data.currentIndex) {
+            this.login();
+        } else {
+            this.loginByToken();
+        }
     },
     login() {
         const { username, password } = this.data;
@@ -32,6 +41,28 @@ Page({
         }).catch(err => {
             wx.clearStorageSync();
             utils.showTip(err);
+        });
+    },
+    loginByToken() {
+        const { token } = this.data;
+        if (!token) return utils.showTip('请输入token!');
+        wx.setStorageSync(CONST.STORAGE_TOKEN, token);
+        wx.navigateBack();
+    },
+    // 复制链接
+    copyLink() {
+        wx.setClipboardData({
+            data: 'https://github.com/settings/tokens/new',
+            success: (result) => {
+                console.log(result)   
+            }
+        });
+    },
+    // 切换登录方式
+    switchTab(e) {
+        const index = parseInt(e.currentTarget.dataset.index);
+        this.setData({
+            currentIndex: index
         });
     }
 });
